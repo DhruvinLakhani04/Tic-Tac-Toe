74a23009f1d4916c7f09be0a91c15e981471a10b
Dhruvin <dhruvinlakhani4@gmail.com>
2025-05-26 19:28:37 +0530

    learn about how to use function and ifelse

---
+++ script.js
@@ -0,0 +1,70 @@
+function getComputerChoice(){
+    let number = Math.random();
+
+    if(number >=0 && number <= 1/3){
+        return "rock";
+    }else if(number >= 1/3 && number <= 2/3){
+        return "paper";
+    }else{
+        return "scissor";
+    }
+}
+
+function getHumanChoice(){
+    return input = prompt("Enter your choice:","");
+}
+
+function playRound(humanChoice,computerChoice){
+        let result;
+
+        let hChoice = humanChoice.toLowerCase();
+        let cChoice = computerChoice.toLowerCase();
+
+        if(hChoice === "rock"){
+            if(cChoice === "rock"){
+                result = "Tie";
+            }else if (cChoice === "paper"){
+                result = "Lose";
+            }else{
+                result = "Win";
+            }
+        }else if(hChoice === "paper"){
+            if(cChoice === "rock"){
+                result = "win";
+            }else if (cChoice === "paper"){
+                result = "Tie";
+            }else{
+                result = "Lose";
+            }
+        }else{
+            if(cChoice === "rock"){
+                result = "Lose";
+            }else if (cChoice === "paper"){
+                result = "Win";
+            }else{
+                result = "Tie";
+            }
+        }
+        alert(`your choice is ${hChoice} and computer choice is ${cChoice}.Result ${result}.`);
+        return result;
+}
+
+function playGame(){
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+for(i=0;i<5;i++){
+    let result = playRound(getHumanChoice(),getComputerChoice());
+
+        if(result === "Win"){
+            humanScore+=1;
+        }else if(result === "Lose"){
+            computerScore+=1;
+        }
+        alert(`computer:${computerScore} Human:${humanScore}`);
+}
+}
+
+playGame();
